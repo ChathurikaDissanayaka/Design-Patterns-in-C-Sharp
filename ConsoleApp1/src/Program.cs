a2b73d81d6a9e22c936ac6cdd683b3e476640a79
Sachini <sachichathudissanayaka@gmail.com>
2024-09-23 21:43:38 +0530

    Add changes

--- ConsoleApp1/src/Program.cs
+++ ConsoleApp1/src/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Encapsulation
-BankAccount bankAccount = new BankAccount(100);
-System.Console.WriteLine(bankAccount.GetBalance());
+// BankAccount bankAccount = new BankAccount(100);
+// System.Console.WriteLine(bankAccount.GetBalance());
 
-bankAccount.Deposit(100);
-System.Console.WriteLine(bankAccount.GetBalance());
+// bankAccount.Deposit(100);
+// System.Console.WriteLine(bankAccount.GetBalance());
 
-bankAccount.Withdraw(100);
-System.Console.WriteLine(bankAccount.GetBalance());
+// bankAccount.Withdraw(100);
+// System.Console.WriteLine(bankAccount.GetBalance());
 // ------------------------------------------------
